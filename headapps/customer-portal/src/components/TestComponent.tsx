c2da2d7d96bd903b675c91b2aefbaa8617f4ecc7
DK\viam <viam@sitecore.net>
2026-06-16 17:47:23 +0300

    add translation to test component

--- headapps/customer-portal/src/components/TestComponent.tsx
+++ headapps/customer-portal/src/components/TestComponent.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 export const Default = (): React.JSX.Element => {
+  const t = useTranslations();
   return (
     <div className="container-default">
       <p>---------TEST TEXT---------</p>
+      <h2>{t('user_register_already_registered_text')}</h2>
     </div>
   );
 };
