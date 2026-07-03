29372ada0a15714ea9de4527cadce3e0ef6a9772
DK\viam <viam@sitecore.net>
2026-07-03 12:11:07 +0300

    change patch file

--- headapps/kit-nextjs-skate-park-132/patch-sitecore.js
+++ headapps/kit-nextjs-skate-park-132/patch-sitecore.js
@@ -6,14 +6,14 @@ const targetFile = path.join(__dirname, '.sitecore', 'import-map.server.ts');
 if (fs.existsSync(targetFile)) {
   let content = fs.readFileSync(targetFile, 'utf8');
   
-  // Replace the broken codegen path with the base package path
+  // Replace the broken core/codegen path with the valid nextjs/codegen module path
   if (content.includes("'@sitecore-content-sdk/core/codegen'")) {
     content = content.replace(
       /'@sitecore-content-sdk\/core\/codegen'/g, 
-      "'@sitecore-content-sdk/nextjs'"
+      "'@sitecore-content-sdk/nextjs/codegen'"
     );
     fs.writeFileSync(targetFile, content, 'utf8');
-    console.log('Successfully patched .sitecore/import-map.server.ts for version 1.3.2!');
+    console.log('Successfully patched .sitecore/import-map.server.ts targeting nextjs/codegen!');
   }
 } else {
   console.log('No generated import map found to patch yet.');
