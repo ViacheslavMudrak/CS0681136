0a43d0ad999f81305b8f9f1b9606397f3b192573
DK\viam <viam@sitecore.net>
2026-07-03 12:35:45 +0300

    change byos component props type

--- headapps/kit-nextjs-skate-park-132/src/byoc/index.client.tsx
+++ headapps/kit-nextjs-skate-park-132/src/byoc/index.client.tsx
@@ -8,6 +8,7 @@ import * as FEAAS from '@sitecore-feaas/clientside/react';
  */
 
 // An important boilerplate component that prevents BYOC components from being optimized away and allows then. Should be kept in this file.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const ClientsideComponent = (props: FEAAS.ComponentProps) => FEAAS.ExternalComponent(props as any);
 /**
  * Clientside BYOC component will be rendered in the browser, so that external components:
